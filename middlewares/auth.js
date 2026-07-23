667c3dce7990e18fd589c1f3bd369d7171f8a800
kzlee <kzlee1612@gmail.com>
2026-07-23 20:37:57 +0800

    feat: authorize neaOfficer role for POST /inspections/schedule

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -40,6 +40,7 @@ function verifyJWT(req, res, next) {
             // NEA officer inspection routes
             "POST /inspections": ["neaOfficer"],
             "PUT /inspections/[0-9]+": ["neaOfficer"],
+            "POST /inspections/schedule": ["neaOfficer"],
 
             // Customer cart routes
             "GET /cart": ["customer"],
